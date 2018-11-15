a0de74264015195667808db42af55cd55aefc4d1
dongyikang <1753547093@qq.com>
2018-11-15 17:12:44 +0800

    添加功能： 专门有协程，可以发送信息 完善功能： 接收服务器端消息

--- classroom_client.go
+++ classroom_client.go
@@ -4,6 +4,9 @@ import (
 	"github.com/astaxie/beego"
 	"net"
 	"fmt"
+	"bufio"
+	"os"
+	"strings"
 )
 
 // 处理错误
@@ -13,12 +16,52 @@ func CheckError(err error){
 	}
 }
 
+// 消息发送协程
+func MessageSend(conn net.Conn){
+	for{
+		// 从控制台读取信息
+		reader := bufio.NewReader(os.Stdin)
+		data,_,_ := reader.ReadLine()
+		input := string(data)
+
+		// 如果信息为exit，则退出
+		if strings.ToUpper(strings.Trim(input, " ")) == "EXIT"{
+			conn.Close()
+			break
+		}
+
+		// 如果信息不为exit,则把信息发往服务器端
+		_,err := conn.Write([]byte(input))
+		if err != nil{
+			conn.Close()
+			fmt.Println("client connect failure:" + err.Error())
+			break
+		}
+	}
+}
+
 func main(){
 	fmt.Println("客户端已启动...")
 	conn, err := net.Dial("tcp", "127.0.0.1:8080")
 	defer conn.Close()
 	CheckError(err)
+	// 得到本地地址和服务器地址
+	localAddr := conn.LocalAddr()
+	localAddrStr := fmt.Sprintf("%s",localAddr)
+	remoteAddr := conn.RemoteAddr()
+	remoteAddrStr := fmt.Sprintf("%s",remoteAddr)
+	fmt.Printf("客户端(%s)已经连接上服务器端(%s)\n", localAddrStr, remoteAddrStr)
 
-	conn.Write([]byte("hello world"))
+	// 开启消息发送协程
+	go MessageSend(conn)
+
+	// 主协程负责接收消息
+	buf := make([]byte, 1024)
+	for{
+		num,_ := conn.Read(buf)
+		if num > 0 {
+			fmt.Println("服务器端(%s): %s", remoteAddr, buf[0:num])
+		}
+	}
 }
 
